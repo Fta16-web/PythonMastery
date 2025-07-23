34c7dbfeda369759c6cb075622b1b2b856a2212e
Fred Talebi <Farhad.talebi.bc@gmail.com>
2025-07-23 16:54:16 -0700

    finishes ch01

--- fastAPI_learning/src/2_book_store_example.py/main.py
+++ fastAPI_learning/src/2_book_store_example.py/main.py
@@ -1,11 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from scalar_fastapi import get_scalar_api_reference
 from router_book import router_book
+from starlette.responses import JSONResponse  # ✅ Fixed import
 
 app = FastAPI()
 
-
 app.include_router(router_book)
+
+
+# custom exception handler for HTTPException
+@app.exception_handler(HTTPException)
+async def http_exception_handler(request, exc):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"message": "Oops! Something went wrong"},
+    )
 
 
 @app.get("/scalar", include_in_schema=False)
